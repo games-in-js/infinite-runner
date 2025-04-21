6d67e6162406027df59c7fde7bc98d533f0fd5ad
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2025-04-21 13:16:37 -0300

    chore: initialize Game canvas

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
 import "./style.css";
+
+class Game {
+  canvas = document.getElementById("game-canvas") as HTMLCanvasElement;
+  ctx = this.canvas.getContext("2d")!;
+
+  constructor() {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+  }
+
+  render() {
+    this.ctx.fillStyle = "#0a0c21";
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+}
+
+window.onload = () => {
+  const game = new Game();
+
+  game.render();
+};
