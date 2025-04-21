e898fb0c6bc103d7d3ea16164e0ea1c2af87432c
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2025-04-21 13:35:30 -0300

    chore: add Player

---
+++ src/entities/Player.ts
@@ -0,0 +1,22 @@
+class Player {
+  constructor(
+    public x: number,
+    public y: number,
+    public width: number,
+    public height: number,
+    public color: string
+  ) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+export default Player;
