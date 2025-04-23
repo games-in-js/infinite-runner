6b93a71cacaa93af901443d03bc420229ad05d1c
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2025-04-23 12:58:55 -0300

    chore: spawn more obstacles

--- src/managers/ObstacleManager.ts
+++ src/managers/ObstacleManager.ts
@@ -2,6 +2,7 @@ import Obstacle from "../entities/Obstacle";
 
 class ObstacleManager {
   obstacles: Array<Obstacle> = [];
+  nextSpawnTime = 0;
 
   constructor(
     private canvas: HTMLCanvasElement,
@@ -9,8 +10,6 @@ class ObstacleManager {
   ) {
     this.canvas = canvas;
     this.ctx = ctx;
-
-    this.createObstacle();
   }
 
   createObstacle() {
@@ -31,8 +30,22 @@ class ObstacleManager {
     });
   }
 
+  // chamado a cada quadro por segundo
+  // 60 fps = 60 quadros por segundo
+  // 60 * 5 = subtrai 300 a cada segundo
+  // 1000 ~ 1000 / 300 ~ 3.33s
+  // 120 fps > 600! => 0.5s
+
+  // um jogo não deve ser mais fácil/mais difícil pelo fps
+  // deltatime
   update() {
-    console.log(this.obstacles.length);
+    this.nextSpawnTime -= 5;
+
+    if (this.nextSpawnTime <= 0) {
+      this.createObstacle();
+
+      this.nextSpawnTime = 300; // 1000ms === 1s
+    }
 
     this.obstacles.forEach((obstacle) => {
       obstacle.update();
