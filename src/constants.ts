298c4222da8a2ca1cc41b02e241c31f52a70deb8
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2025-04-24 20:41:22 -0300

    chore: add sprite to the ground

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,5 @@ export const SPAWN_MAX_TIME = 2500;
 
 export const GROUND_HEIGHT = 80;
 export const GROUND_COLOR = "#251614";
+
+export const GROUND_TILE_SIZE = 24;
--- src/entities/Ground.ts
+++ src/entities/Ground.ts
@@ -1,16 +1,33 @@
-import { GROUND_COLOR } from "../constants";
+import { GROUND_COLOR, GROUND_TILE_SIZE } from "../constants";
 
 class Ground {
+  private sprite: HTMLImageElement;
+
   constructor(
     private x: number,
     private y: number,
     private width: number,
     private height: number
-  ) {}
+  ) {
+    this.sprite = new Image();
+    this.sprite.src = "ground.png";
+  }
 
   draw(ctx: CanvasRenderingContext2D) {
+    const tilesX = Math.ceil(this.width / GROUND_TILE_SIZE);
+
     ctx.fillStyle = GROUND_COLOR;
     ctx.fillRect(this.x, this.y, this.width, this.height);
+
+    for (let i = 0; i < tilesX; i++) {
+      ctx.drawImage(
+        this.sprite,
+        this.x + i * GROUND_TILE_SIZE, // 0 | 0 + 1 * 24 | 0 + 2 * 24
+        this.y,
+        GROUND_TILE_SIZE,
+        GROUND_TILE_SIZE
+      );
+    }
   }
 }
 
